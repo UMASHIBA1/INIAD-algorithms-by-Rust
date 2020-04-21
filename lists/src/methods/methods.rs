21c9f10aa9a35400e448d1b6d8a4e6ae29d15bf7
UMASHIBA <umashiba1@gmail.com>
2020-04-22 00:55:33 +0900

    [MOD]ListMethodsを正しいtraitの書き方に変更

--- lists/src/methods/methods.rs
+++ lists/src/methods/methods.rs
@@ -1,16 +1,15 @@
-trait ListMethod {
+pub trait ListMethods {
     type LIST;
     type ELEMENT;
     fn construct(&self) -> &Self::LIST;
     fn list_free(&self) -> ();
     fn list_print(&self) -> ();
-    fn list_insert(&self, usize, &Self::ELEMENT) -> ();
-    fn list_delete(&self, usize) -> ();
+    fn list_insert(&self, pos: usize, value: &Self::ELEMENT) -> ();
+    fn list_delete(&self, pos: usize) -> ();
     fn list_first(&self) -> usize;
     fn list_last(&self) -> usize;
-    fn list_next(&self, usize) -> usize;
-    fn list_previous(&self, usize) -> usize;
-    fn list_get(&self, usize) -> &Self::ELEMENT;
-    fn list_index(&self, &Self::ELEMENT) -> usize;
+    fn list_next(&self, pos: usize) -> usize;
+    fn list_previous(&self, pos: usize) -> usize;
+    fn list_get(&self, pos: usize) -> &Self::ELEMENT;
+    fn list_index(&self, pos: &Self::ELEMENT) -> usize;
 }
-
